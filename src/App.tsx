454f79f3b9db1c160cae7112c8ca3faf3822bd68
Zet <111925382+zetmosoma10@users.noreply.github.com>
2024-07-24 20:20:01 +0200

    build Design card

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
+import DesignCard from "./components/DesignCard";
 import AboutHero from "./pages/about/components/AboutHero";
 import DealCard from "./pages/about/components/DealCard";
 
 function App() {
   return (
-    <div className="p-20">
-      <AboutHero />
+    <div className="p-10">
+      <DesignCard />
     </div>
   );
 }
