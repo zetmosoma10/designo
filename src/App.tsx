ce8d7cb5676f0e69e1a063ae4deb641ca54a213a
Zet <111925382+zetmosoma10@users.noreply.github.com>
2024-07-26 15:45:19 +0200

    implement validation using react-hook-form with zod

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
+import Contacts from "./pages/contact/sections/Contacts";
 import LocationDetails from "./pages/locations/components/LocationDetails";
 import LocationLayoutComponent from "./pages/locations/components/LocationLayoutComponent";
 
 function App() {
   return (
     <div>
-      <LocationLayoutComponent />
+      <Contacts />
     </div>
   );
 }
--- src/pages/contact/components/Form.tsx
+++ src/pages/contact/components/Form.tsx
@@ -1,33 +1,80 @@
-import React from "react";
+import { FieldValues, useForm } from "react-hook-form";
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod/dist/zod.js";
 import Button from "../../../components/Button";
 import errorIcon from "../../../assets/contact/desktop/icon-error.svg";
 
+const schema = z.object({
+  name: z.string().min(3, { message: "Name must at least 3 characters." }),
+  email: z
+    .string()
+    .min(1, { message: "Email can't be empty" })
+    .email({ message: "Invalid email address" }),
+  phone: z.string(),
+  message: z.string(),
+});
+
+type FormData = z.infer<typeof schema>;
+
 const Form = () => {
+  const {
+    register,
+    handleSubmit,
+    reset,
+    formState: { errors },
+  } = useForm<FormData>({ resolver: zodResolver(schema) });
+
+  const onSubmit = (data: FieldValues) => {
+    //  toast(`${data.name} thanks for your support, we’ll get in touch shortly.`);
+    reset();
+  };
   return (
-    <form className="flex flex-col items-start gap-6">
+    <form
+      onSubmit={handleSubmit(onSubmit)}
+      className="flex flex-col items-start gap-6"
+    >
       <div className="relative w-full flex items-center">
-        <input className="custom-input" placeholder="Name" type="text" />
-        <small className="absolute right-2 text-white text-nowrap flex items-center gap-2">
-          {" "}
-          <span>Can't be empty</span> <img src={errorIcon} alt="" />
-        </small>
+        <input
+          {...register("name")}
+          className="custom-input"
+          placeholder="Name"
+          type="text"
+        />
+        {errors.name && (
+          <small className="absolute right-2 text-white text-nowrap flex items-center gap-2">
+            {" "}
+            <span>{errors.name.message}</span> <img src={errorIcon} alt="" />
+          </small>
+        )}
       </div>
       <div className="relative w-full flex items-center">
-        <input className="custom-input" placeholder="Email" type="email" />
-        <small className="absolute right-2 text-white text-nowrap flex items-center gap-2">
-          {" "}
-          <span>Can't be empty</span> <img src={errorIcon} alt="" />
-        </small>
+        <input
+          {...register("email")}
+          className="custom-input"
+          placeholder="Email"
+          type="email"
+        />
+        {errors.email && (
+          <small className="absolute right-2 text-white text-nowrap flex items-center gap-2">
+            {" "}
+            <span>{errors.email.message}</span> <img src={errorIcon} alt="" />
+          </small>
+        )}
       </div>
       <div className=" relative w-full flex items-center">
-        <input className="custom-input" placeholder="Phone" type="text" />
-        <small className="absolute right-2 text-white text-nowrap flex items-center gap-2">
-          {" "}
-          <span>Can't be empty</span> <img src={errorIcon} alt="" />
-        </small>
+        <input
+          {...register("phone")}
+          className="custom-input"
+          placeholder="Phone"
+          type="text"
+        />
       </div>
       <div className="w-full">
-        <textarea className="custom-input" placeholder="Message"></textarea>
+        <textarea
+          {...register("message")}
+          className="custom-input"
+          placeholder="Message"
+        ></textarea>
       </div>
       <Button type="button" className="self-center md:self-end px-12 ">
         Submit
