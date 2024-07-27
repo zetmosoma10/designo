454a16cd7a59852fbac5f1850897ef09b5bc76c1
Zet <111925382+zetmosoma10@users.noreply.github.com>
2024-07-27 18:54:06 +0200

    build a Web Design Page

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,12 @@ import AppDesignPage from "./pages/app-design/AppDesignPage";
 import GraphicDesignPage from "./pages/graphic-design/GraphicDesignPage";
 import HomePage from "./pages/home/HomePage";
 import LocationPage from "./pages/locations/LocationPage";
+import WebDesignPage from "./pages/web-design/WebDesignPage";
 
 function App() {
   return (
     <div className="">
-      <GraphicDesignPage />
+      <WebDesignPage />
     </div>
   );
 }
---
+++ src/pages/web-design/WebDesignPage.tsx
@@ -0,0 +1,43 @@
+import React from "react";
+import DesignCard from "../../components/DesignCard";
+import DesignMultiGrid from "../../components/DesignMultiGrid";
+import DesignsHeader from "../../components/DesignsHeaders/DesignsHeader";
+import DesignsText from "../../components/DesignsHeaders/DesignsText";
+import DesignsTitle from "../../components/DesignsHeaders/DesignsTitle";
+import DesignShowCard from "../../components/DesignShowCard";
+import { webDesignData } from "./constance";
+
+const WebDesignPage = () => {
+  return (
+    <div className="space-y-[120px] md:space-y-[160px]">
+      <DesignsHeader>
+        <DesignsTitle>Web Design</DesignsTitle>
+        <DesignsText>
+          We build websites that serve as powerful marketing tools and bring
+          memorable brand experiences.
+        </DesignsText>
+      </DesignsHeader>
+      <DesignMultiGrid>
+        {webDesignData.map((card) => (
+          <DesignCard key={card.header} {...card} />
+        ))}
+      </DesignMultiGrid>
+      <div className="grid lg:grid-cols-2 gap-x-7 gap-y-6 max-container">
+        <DesignShowCard
+          title="Graphic Designs"
+          mobileBgImg="bg-graphicMobileImg"
+          tabletBgImg="md:bg-graphicTabletImg"
+          desktopBgImg="lg:bg-graphicDesktopImg"
+        />
+        <DesignShowCard
+          title="App Designs"
+          mobileBgImg="bg-appMobileImg"
+          tabletBgImg="md:bg-appTabletImg"
+          desktopBgImg="lg:bg-appDesktopImg"
+        />
+      </div>
+    </div>
+  );
+};
+
+export default WebDesignPage;
